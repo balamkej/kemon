b942db3518abfae087a1c08c3ac32b3dbac8b36a
Robert Henderson <balamkej@gmail.com>
2018-06-06 14:58:03 -0700

    Switch to Spinbox in place of Entry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import tkinter as tk
+from tkinter.font import Font
 
 root = tk.Tk()
 
@@ -8,7 +9,32 @@ height = 5
 width = 5
 for i in range(height): #Rows
     for j in range(width): #Columns
-        b = tk.Entry(root, text="")
-        b.grid(row=i, column=j)
+        b = tk.Spinbox(root, from_=0, to=1, width=1,
+                       font=Font(family='Helvetica',
+                       size=20,
+                       weight='bold'))
+        b.grid(row=i, column=j, padx=10, pady=10)
 
 tk.mainloop()
+
+
+
+# Warp Grid
+
+# Weft Grid
+
+# Tie-up
+
+# Color Palette
+
+# Menu
+
+
+
+
+
+#from tkinter.colorchooser import *
+#def getColor():
+#    color = askcolor() 
+#    print color
+#Button(text='Select Color', command=getColor).pack()
