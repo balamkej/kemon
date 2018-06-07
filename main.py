86509db3d79b63db1f62f4c80557234a9924ccf9
Robert Henderson <balamkej@gmail.com>
2018-06-07 09:44:33 -0700

    Add frame for warp grid

--- main.py
+++ main.py
@@ -2,24 +2,33 @@
 
 import tkinter as tk
 from tkinter.font import Font
+from PIL import ImageTk, Image
 
 root = tk.Tk()
+root.title("Kemon")
+root.geometry("100x100")
+
+path = "resources/icon.jpg"
+img = ImageTk.PhotoImage(Image.open(path))
+panel = tk.Label(root, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+
+
+# Warp Grid
+
+warp = tk.Toplevel()
+warp.title("Warp Grid")
 
 height = 5
 width = 5
 for i in range(height): #Rows
     for j in range(width): #Columns
-        b = tk.Spinbox(root, from_=0, to=1, width=1,
+        b = tk.Spinbox(warp, from_=0, to=1, width=1,
                        font=Font(family='Helvetica',
                        size=20,
                        weight='bold'))
         b.grid(row=i, column=j, padx=10, pady=10)
 
-tk.mainloop()
-
-
-
-# Warp Grid
 
 # Weft Grid
 
@@ -30,7 +39,7 @@ tk.mainloop()
 # Menu
 
 
-
+root.mainloop()
 
 
 #from tkinter.colorchooser import *
