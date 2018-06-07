0332b741e25a1d03eab4c0aa6f8685a5cbb0ed3e
Robert Henderson <balamkej@gmail.com>
2018-06-07 10:03:01 -0700

    Add weft grid

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ warp = tk.Toplevel()
 warp.title("Warp Grid")
 
 height = 5
-width = 5
+width = 10
 for i in range(height): #Rows
     for j in range(width): #Columns
         b = tk.Spinbox(warp, from_=0, to=1, width=1,
@@ -31,6 +31,20 @@ for i in range(height): #Rows
 
 
 # Weft Grid
+
+weft = tk.Toplevel()
+weft.title("Weft Grid")
+
+height = 10
+width = 5
+for i in range(height): #Rows
+    for j in range(width): #Columns
+        b = tk.Spinbox(weft, from_=0, to=1, width=1,
+                       font=Font(family='Helvetica',
+                       size=20,
+                       weight='bold'))
+        b.grid(row=i, column=j, padx=10, pady=10)
+
 
 # Tie-up
 
