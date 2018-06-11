e095a095f35904d4f14ae03c969e243c3e6c35ab
Robert Henderson <balamkej@gmail.com>
2018-06-11 09:07:48 -0700

    Switch from matplotlib to PIL.

--- weaving.py
+++ weaving.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+From PIL import Image
 
 def mushingIndex(treadleRow):
     return [i for i, j in enumerate(treadleRow) if j == 1]
@@ -22,7 +22,7 @@ def weave(treadle, threading, tieup):
     pattern = np.zeros((len(treadle[:,0]),len(threading[0])), int)
     for i in range(len(treadle[:,0])):
         pattern[i] = mushing(threading, mushing2(treadle[i],tieup))
-    return pattern
+    return Image.fromarray(pattern)
 
         
 #            treadleArray = np.genfromtxt(treadle, delimiter=",")
